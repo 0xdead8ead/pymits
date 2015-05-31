ef017a4025efde7b43209810f7d5ce9629cd0582
f47h3r <f47h3r@0x539.us>
2015-05-30 17:32:00 -0700

    Cleaned up getter methods.

--- opendsd.py
+++ opendsd.py
@@ -3,45 +3,83 @@
 import requests
 import json
 
+
 class OpenDSD():
     """OpenDSD Python Library"""
     def __init__(self):
         pass
 
     def getCodeEnforce(self, id):
+        ''' Get Code Enforcement Complaint by ID '''
         url = 'http://opendsd.sandiego.gov/api/codeenforcement/'+id
         headers = {'Accept': 'application/json'}
-        #data = ''
-
         r = requests.get(url, headers=headers)
-        #print r.text
-        complainObject = json.loads(r.text)
-        if('Description' in complainObject):
-            print complainObject['Description']
-            print "\n"
+        complaintObject = json.loads(r.text)
+        
+        print r.text
+        
+        if(complaintObject.get('ErrorMessage') is None):
+            return None
         else:
-            pass
+            return complaintObject
 
     def getProject(self, id):
+        ''' Get Project by ID '''
         url = 'http://opendsd.sandiego.gov/api/project/'+id
         headers = {'Accept': 'application/json'}
-        #data = ''
+
         r = requests.get(url, headers=headers)
+        projectObject = json.loads(r.text)
+        
         print r.text
+        
+        if(projectObject.get('ErrorMessage') is None):
+            return None
+        else:
+            return projectObject
 
     def getApproval(self, id):
+        ''' Get Approval by ID '''
         url = 'http://opendsd.sandiego.gov/api/approval/'+id
         headers = {'Accept': 'application/json'}
-        #data = ''
+
         r = requests.get(url, headers=headers)
+        approvalObject = json.loads(r.text)
+        
         print r.text
+        
+        if(approvalObject.get('ErrorMessage') is None):
+            return None
+        else:
+            return approvalObject
 
     def getInvoice(self, id):
+        ''' Get Invoice by ID '''
         url = 'http://opendsd.sandiego.gov/api/project/'+id
         headers = {'Accept': 'application/json'}
         #data = ''
         r = requests.get(url, headers=headers)
+        invoiceObject = json.loads(r.text)
+
         print r.text
+
+        if(invoiceObject.get('ErrorMessage') is None):
+            return None
+        else:
+            return invoiceObject
+
+    def getComplaintsByGPS(self):
+        url = 'http://opendsd.sandiego.gov/api/CeCaseMapSearch/?SouthWestLatitude=32.71879985593221&SouthWestLongitude=-117.16525563507082&NorthEastLatitude=32.74399836325726&NorthEastLongitude=-117.12534436492922'
+        headers = {'Accept': 'application/json'}
+
+        r = requests.get(url, headers=headers)
+        regionalComplaints = json.loads(r.text)
+        print r.text
+
+        if(regionalComplaints.get('ErrorMessage') is None):
+            return None
+        else:
+            return regionalComplaints
 
     def scrapeComplaints(self):
         for id in range(200000, 222222):
@@ -51,4 +89,6 @@ class OpenDSD():
 if __name__ == '__main__':
     dsd = OpenDSD()
     #dsd.getCodeEnforce('122556')
-    dsd.scrapeComplaints()
+    #dsd.scrapeComplaints()
+    dsd.getComplaintsByGPS()
+
